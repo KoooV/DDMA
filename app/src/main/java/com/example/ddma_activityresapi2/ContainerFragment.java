d4114482107b8d0caeb030824040ce849da58a96
KoV <alexkovalev13681@gmail.com>
2025-03-25 14:06:49 +0300

    changes to fragment classes and their components

---
+++ app/src/main/java/com/example/ddma_activityresapi2/ContainerFragment.java
@@ -0,0 +1,23 @@
+package com.example.ddma_activityresapi2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class ContainerFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_container, container, false);
+    }
+}
--- app/src/main/java/com/example/ddma_activityresapi2/MainActivity.java
+++ app/src/main/java/com/example/ddma_activityresapi2/MainActivity.java
@@ -1,40 +1,55 @@
 package com.example.ddma_activityresapi2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-    private final int fragmentContainerId = R.id.fragment_container;
+    private boolean isDynamicFragmentShowing = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Инициализация FragmentC в ContainerView
-        if (savedInstanceState == null) {
+
+        // Инициализация первого фрагмента
+        if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container_view, new FragmentC())
+                    .add(R.id.fragmentContainer, new DynamicFragment())
                     .commit();
         }
-    }
 
-    public void navigateToFragmentB() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(fragmentContainerId, new FragmentB())
-                .addToBackStack(null)
-                .commit();
-    }
+        Button switchButton = findViewById(R.id.switchButton);
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchFragments();
+            }
+        });
+        }
 
-    public void navigateToFragmentC() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(fragmentContainerId, new FragmentC())
-                .addToBackStack(null)
-                .commit();
+    private void switchFragments() {
+        Fragment newFragment;
+        if(isDynamicFragmentShowing) {
+            newFragment = new ContainerFragment();
+        } else {
+            newFragment = new DynamicFragment();
+        }
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, newFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        isDynamicFragmentShowing = !isDynamicFragmentShowing;
     }
     }
